0481fa8ccfb66ca9c8b183a855f5018983692e64
Yasser Sallam (Yasserious) <51271928+yasseriouss@users.noreply.github.com>
2026-05-12 06:37:38 +0300

    yalla

---
+++ index.ts
@@ -0,0 +1,1 @@
+import app from "./server/src/app"; export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-// @ts-ignore
-import app from "../server/src/app";
-
-// Vercel expects the express app to be exported as a handler for serverless functions
-export default app;
+import app from "../server/src/app"; export default app;
