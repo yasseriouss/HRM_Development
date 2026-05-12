264c6e4323940a2ee75b59cb6085081ba279fc25
Yasser Sallam (Yasserious) <51271928+yasseriouss@users.noreply.github.com>
2026-05-12 14:55:04 +0300

    aywaa

--- src/App.tsx
+++ src/App.tsx
@@ -126,21 +126,15 @@ function AppRoutes() {
               <Route path="/spreadsheet" component={() => <ProtectedRoute component={SpreadsheetPage} roles={ADMIN_HR_DEPT} />} />
 
               {/* Docs */}
-              <Route path="/docs/:rest*">
-                {() => (
-                  <WouterRouter base="/docs">
-                    <ProtectedRoute component={DocsApp} roles={ALL_ROLES} />
-                  </WouterRouter>
-                )}
+              <Route path="/docs" nest>
+                <ProtectedRoute component={DocsApp} roles={ALL_ROLES} />
               </Route>
               
               {/* Interactive Presentation */}
-              <Route path="/interactive-presentation/:rest*">
-                {() => (
-                  <WouterRouter base="/interactive-presentation">
-                    <ProtectedRoute component={InteractivePresentationApp} roles={ALL_ROLES} />
-                  </WouterRouter>
-                )}
+              <Route path="/interactive-presentation" nest>
+                <WouterRouter base="/interactive-presentation">
+                  <ProtectedRoute component={InteractivePresentationApp} roles={ALL_ROLES} />
+                </WouterRouter>
               </Route>
 
               <Route path="/my-profile" component={() => <ProtectedRoute component={MyProfilePage} roles={ALL_ROLES} />} />
